b023769aa50fd7be444e5743bf7b753c8ab47b37
iamjink <jinijinjin@gmail.com>
2020-01-27 00:11:21 -0800

    pseudocoding in app.js

--- assets/app.js
+++ assets/app.js
@@ -1,11 +1,30 @@
 $(document).ready(function(){
 
+    //start button
 
 
-document.getElementById("button").onclick = function() {
-    console.log("clicked")
-    document.getElementById("button").innerHTML = "bye!!!"
-};
+//start timer
+function timerCountdown() {
+    console.log("time countdonw starts");
+    $("#timerText").text("15");
+}
+
+timerCountdown()
+
+//show question in quest{ionText div
+
+
+//make buttons for answerChoices
+
+//if onclick === answers, then show "correct!", then go to the next question
+
+//when all the questions are answered, then show # of wrongs and # of corrects, 
+
+//then display play again button
+
+
+
+
 
 
 
