7da58f2b4c15f58b61b832e8b06cb1d407115cfc
iamjink <jinijinjin@gmail.com>
2020-01-26 22:51:52 -0800

    initial design in css and html. no javascript yet

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,22 @@
+$(document).ready(function(){
 
+
+
+document.getElementById("button").onclick = function() {
+    console.log("clicked")
+    document.getElementById("button").innerHTML = "bye!!!"
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
