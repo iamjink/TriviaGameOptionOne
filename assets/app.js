65db9a26a945be52aedbeaaecde8d21d646c0c26
iamjink <jinijinjin@gmail.com>
2020-01-27 05:06:03 -0800

    setting the reset button in app.js

--- assets/app.js
+++ assets/app.js
@@ -1,53 +1,54 @@
 $(document).ready(function () {
+
     var QOptions = [{
             question: "Which river goes through London?",
             choices: ["Thames", "Ganges", "Yellow River", "River Severn"],
-            answer: 1
+            answer: 0
         },
         {
             question: "When was President Kennedy killed?",
             choices: ["1942", "1961", "1963", "1955"],
-            answer: 3
+            answer: 2
         },
         {
             question: "What’s the smallest type of tree in the world?",
             choices: ["Ash", "Birch", "Bonsai", "Acacia"],
-            answer: 3
+            answer: 2
         },
         {
             question: "What’s the smallest type of tree in the world?",
             choices: ["Ash", "Birch", "Bonsai", "Acacia"],
-            answer: 3
+            answer: 2
         },
         {
             question: "What’s the smallest type of tree in the world?",
             choices: ["Ash", "Birch", "Bonsai", "Acacia"],
-            answer: 3
+            answer: 2
         },
         {
             question: "What’s the smallest type of tree in the world?",
             choices: ["Ash", "Birch", "Bonsai", "Acacia"],
-            answer: 3
+            answer: 2
         },
         {
             question: "What’s the smallest type of tree in the world?",
             choices: ["Ash", "Birch", "Bonsai", "Acacia"],
-            answer: 3
+            answer: 2
         },
         {
             question: "What’s the smallest type of tree in the world?",
             choices: ["Ash", "Birch", "Bonsai", "Acacia"],
-            answer: 3
+            answer: 2
         },
         {
             question: "What’s the smallest type of tree in the world?",
             choices: ["Ash", "Birch", "Bonsai", "Acacia"],
-            answer: 3
+            answer: 2
         },
         {
             question: "What’s the smallest type of tree in the world?",
             choices: ["Ash", "Birch", "Bonsai", "Acacia"],
-            answer: 3
+            answer: 2
         },
     ];
 
@@ -55,12 +56,14 @@ $(document).ready(function () {
     var clockRunning = false;
     var time = 15;
     var intervalId;
-
-
+    var newIndex;
+    var currentQuestion;
+    var userGuess = "";
+    var correctCount = 0;
+    var wrongCount = 0;
 
     //start button
     function reset() {
-
         //shows start button 
 
         var startButton = $("<img>")
@@ -70,36 +73,10 @@ $(document).ready(function () {
 
         //user clicks start button, and first question shows in div
 
-        $("#start-button").on("click", gameStart);
+        $("#start-button").on("click", questionDisplay);
 
     };
 
-    //start button on click function; starts the questions
-    function gameStart() {
-        timerStart();
-        questionDisplay();
-
-    }
-
-    function questionDisplay() {
-
-        //randomizing objects in question array and creating a new randomized question array called current question
-        newIndex = Math.floor(Math.random() * QOptions.length);
-        currentQuestion = QOptions[newIndex];
-
-        //displaying new question
-
-        $("#questionText").text(currentQuestion.question);
-
-        for (var i = 0; i < currentQuestion.choices.length; i++) {
-            choices = $("<p>");
-            choices.text(currentQuestion.choices[i]);
-            choices.attr("val", i);
-            $("#answerChoices").append("<br>" + currentQuestion.choices[i]);
-
-        };
-
-    }
 
     //timer functions
     function timerStart() {
@@ -109,8 +86,6 @@ $(document).ready(function () {
             console.log("time countdonw starts now");
             clockRunning = true;
         }
-
-
     }
 
     function decrement() {
@@ -125,39 +100,69 @@ $(document).ready(function () {
     function stop() {
         clockRunning = false;
         clearInterval(intervalId);
+        time = 15;
     }
 
-    //when user answers by clicking
- 
-        $("#answerChoices").on("click", function() {
-            userChoice = parseInt($(this).attr("val"));
-            
-            console.log(userChoice);
-            if (userChoice === currentQuestion.answer) {
-                
+
+
+    function questionDisplay() {
+
+        //randomizing objects in question array and creating a new randomized question array called current question
+        newIndex = Math.floor(Math.random() * QOptions.length);
+        currentQuestion = QOptions[newIndex];
+
+        //displaying new question
+        timerStart();
+
+        $("#questionText").text(currentQuestion.question);
+
+        $("#answerChoices").empty();
+
+
+        for (var i = 0; i < currentQuestion.choices.length; i++) {
+
+            var userGuess = $("<div>");
+            userGuess.addClass("answerBlock");
+            userGuess.html(currentQuestion.choices[i]);
+            userGuess.attr("value", i);
+            $("#answerChoices").append(userGuess);
+        }
+
+        $(".answerBlock").on("click", function () {
+
+            userGuess = ($(this).attr("value"));
+
+            if (userGuess == currentQuestion.answer) {
+                stop();
+                correctCount++;
+                console.log("correct!")
+                questionDisplay();
+
+            } else {
+                stop();
+                wrongCount++;
+                console.log("incorrect!")
+                questionDisplay();
             }
-            
+
+            if ((correctCount + wrongCount) == QOptions.length) {
+                $("#questionText").text("You answered " + correctCount + " correct!")
+                $("#answerChoices").empty();
+
+                //reset button
+                var startButton = $("<img>")
+                startButton.attr("src", "assets/images/resetButton.png");
+                startButton.attr("id", "start-button")
+                $("#questionText").append(startButton);
+                $("#start-button").on("click", questionDisplay);
+            }
+
         });
 
-        
+    }
+
+
     reset();
-
-
-
-
-
-
-    //show question in quest{ionText div
-
-
-    //make buttons for answerChoices
-
-    //if onclick === answers, then show "correct!", then go to the next question
-
-    //when all the questions are answered, then show # of wrongs and # of corrects, 
-
-    //then display play again button
-
 
 
 
